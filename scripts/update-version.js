e2eb1788eb363d748da60546d6a21dc19c9a1b73
Dobrin Dimchev <dobrin.dimchev@sap.com>
2021-12-20 09:54:12 +0200

    fix: path

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 const content = args[0]
 
 try {
-  fs.writeFileSync('./test.txt', content)
+  fs.writeFileSync('../dist/test.txt', content)
   //file written successfully
 } catch (err) {
   console.error(err)
